94d16af0a8cced695947c6dbbfa92060d63d2a9f
Daniel Guzman <guzdan@live.com>
2021-10-13 20:37:09 -0500

    commit tree fb4ccf1a75ba05534dd80bd422a5d3b3c5ebe4f8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+
+    const hqStreet = 42;
+    let blocksDistance = Math.abs(location - hqStreet);
+    return blocksDistance;
+}
+
+function distanceFromHqInFeet(location){
+
+    let feetDistance =  distanceFromHqInBlocks(location) * 264  
+    return feetDistance;
+
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+
+    let feetTraveled = (Math.abs(endBlock - startBlock)) * 264;
+    return feetTraveled;
+
+}
+
+function calculatesFarePrice(start, end){
+    let feetTraveled = distanceTravelledInFeet(start,end);
+    let fare;
+   
+    if(feetTraveled <= 400){
+        fare = 0;
+    }
+    else if(feetTraveled >= 401 && feetTraveled <= 2000){
+        fare = (feetTraveled - 400) * 0.02;
+    }
+    else if(feetTraveled > 2000 && feetTraveled < 2500){
+        fare = 25;
+    }
+    else if(feetTraveled >= 2500){
+        return ("cannot travel that far")
+    }
+
+    return fare;
+}
